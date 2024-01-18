a7d0113979918bfd4329416f51006ead64149f57
kaleopiasse <kaleopiasse@tokenlab.com.br>
2024-01-18 15:50:53 -0300

    feat(project): add paths organization project

---
+++ src/environments/environment.prd.ts
@@ -0,0 +1,10 @@
+import { Environment } from './environment.model';
+
+export const environment: Environment = {
+  production: true,
+
+  apiUser: 'https://user.prd.tokenlab.com.br',
+
+  cognitoUserPoolId: 'myCognitoUserPoolId',
+  cognitoAppClientId: 'myCognitoAppClientId',
+};
