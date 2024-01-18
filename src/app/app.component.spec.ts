b3cd17e096b3b12ddc8a4b4602d5d8a66d964755
kaleopiasse <kaleopiasse@tokenlab.com.br>
2024-01-18 12:18:49 -0300

    feat: add husky pre commit, fix prettier

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,27 +3,35 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+    }),
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'update-template'`, () => {
+  it('should have as title update-template', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app.title).toEqual('update-template');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
+
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('update-template app is running!');
+
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'update-template app is running!',
+    );
   });
 });
